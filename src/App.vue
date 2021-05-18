7184bc0d96c2e96d0da6aa96edfa1663fd1d5f41
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-05-18 23:28:10 +0900

    Added marked library and converted uploaded md file to html

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <div id="app">
-    <input type="file">
+    <input type="file" @change="handleFile" accept=".md" />
+    <div v-if="markdown" v-html="html"></div>
   </div>
 </template>
 
 <script>
+import marked from "marked";
 
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      markdown: "",
+      html:""
+    };
+  },
+  methods: {
+    handleFile(event) {
+      const file  = event.target.files[0];
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        this.markdown = e.target.result;
+      });
+      reader.addEventListener("loadend", ()=> {
+        this.html = this.convertToHTML(this.markdown);
+      });
+      reader.readAsBinaryString(file);
+    },
+    convertToHTML(md) {
+      marked.setOptions({
+        // Creates <br> for line carriages (new lines)
+        gfm: true,
+        breaks: true,
+      });
+      return marked(md);
+    },
+  },
+};
 </script>
 
 <style>
