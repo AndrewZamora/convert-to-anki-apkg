3307fe510a3cbdad2565e86b32fc6cf6a60eccf8
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-06-16 23:00:37 +0900

    Added app bar

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,20 @@
 <template>
   <v-app>
     <div>
-      <router-view></router-view>
+      <v-app-bar dense>
+        <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+        <v-toolbar-title>Convert to Anki Deck</v-toolbar-title>
+      </v-app-bar>
     </div>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
 // import Tiptap from "./components/tiptap.vue";
-
 </script>
 
 <style>
 #app {
-}
-.textarea {
-  height: 50vh;
-  width: 100%;
-  box-sizing: border-box;
-  resize: none;
-  font-size: 20px;
-  border: solid 2px black;
-}
-.textarea-container {
-  display: flex;
-  margin-bottom: 10px;
-}
-
-.textarea-title {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.upload {
-  display: flex;
-  align-items: center;
-  flex: 1;
-  justify-content: center;
-}
-.editor {
-  flex: 1;
-  margin-right: 2em;
-}
-
-.preview-container {
-  flex: 1;
-}
-
-.preview {
-  flex: 1;
-  word-break: break-all;
-  overflow-y: auto;
-  hyphens: auto;
-  word-wrap: break-word;
-  height: 50vh;
-  border: solid 2px black;
-}
-
-.preview h2 {
-  border-bottom: black 2px solid;
-  text-align: center;
-}
-
-@media screen and (max-width: 500px) {
-  .textarea-container {
-    flex-direction: column-reverse;
-  }
-  .preview {
-    height: 25vh;
-  }
-  .textarea {
-    height: 25vh;
-  }
-  .editor {
-    margin-right: 0;
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <v-container>
     <v-tabs>
       <v-tab @click="currentTab = 'editor'">Editor</v-tab>
       <v-tab @click="currentTab = 'upload'">Upload</v-tab>
@@ -26,7 +26,7 @@
     <div>
       <v-btn color="primary" elevation="2" @click="submit"> Create Deck</v-btn>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -103,4 +103,72 @@ export default {
 </script>
 
 <style>
+
+.textarea {
+  height: 50vh;
+  width: 100%;
+  box-sizing: border-box;
+  resize: none;
+  font-size: 20px;
+  border: solid 2px black;
+}
+.textarea-container {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.textarea-title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.upload {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+}
+.editor {
+  flex: 1;
+  margin-right: 2em;
+}
+
+.preview-container {
+  flex: 1;
+}
+
+.preview {
+  flex: 1;
+  word-break: break-all;
+  overflow-y: auto;
+  hyphens: auto;
+  word-wrap: break-word;
+  height: 50vh;
+  border: solid 2px black;
+}
+
+.preview h2 {
+  border-bottom: black 2px solid;
+  text-align: center;
+}
+
+@media screen and (max-width: 500px) {
+  .textarea-container {
+    flex-direction: column-reverse;
+  }
+  .preview {
+    height: 25vh;
+  }
+  .textarea {
+    height: 25vh;
+  }
+  .upload {
+    flex:none;
+    height: 25vh;
+  }
+  .editor {
+    margin-right: 0;
+  }
+}
 </style>
